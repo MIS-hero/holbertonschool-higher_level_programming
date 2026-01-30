241fdd125dd7c06898e1a35e35c0aae699be4039
Mis-hero <12423@holbertonstudents.com>
2026-01-30 18:57:27 +0300

    task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for item in my_list:
-        if item == search:
-            Index = my_list.index(search)
-            my_list[Index] = replace
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            my_list[i] = replace
     return my_list
