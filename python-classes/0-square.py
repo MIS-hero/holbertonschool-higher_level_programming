c339f81e965bd7a24b3fb907906333ca972622ac
Mis-hero <12423@holbertonstudents.com>
2026-01-31 20:14:17 +0300

    doc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines the User class and related helper functions
+for managing user information in the application.
+"""
+
+
 class Square:
     pass
+    '''empty class Square that defines a square'''
