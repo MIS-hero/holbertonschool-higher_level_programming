4417f9685c1ce10b624517d6999bd8de3235e211
Mis-hero <12423@holbertonstudents.com>
2026-01-31 20:43:09 +0300

    task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+This module defines the User class and related helper functions
+for managing user information in the application.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+    def __init__(self, size):
+        '''
+        Initialize a square with a given size.
+
+        Args:
+            size (int): The size of the square's sides. Default is 0.
+        '''
+        self.__size = size
